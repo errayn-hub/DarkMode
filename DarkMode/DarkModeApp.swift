cd4c0d00d4a5de3c26b7b21430b5987d5fb08214
errayn-hub <128374386+errayn-hub@users.noreply.github.com>
2023-04-27 16:52:57 +0300

    Initial Commit

---
+++ DarkMode/DarkModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkModeApp.swift
+//  DarkMode
+//
+//  Created by ErRayn on 27.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct DarkModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
